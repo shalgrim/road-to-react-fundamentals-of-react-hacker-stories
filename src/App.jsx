c6e8b2b7788f314112011618febca5d74915c888
Scott Halgrim <shalgrim@gmail.com>
2025-04-11 11:24:20 -0700

    screwed up props even more

--- src/App.jsx
+++ src/App.jsx
@@ -18,26 +18,27 @@ const list = [
   }
 ];
 
-function Item({item}) {
+function Item({ item }) {
   return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
+    <li key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
   );
 }
 
 function List() {
   return (
     <ul>
-      {list.map(function (item) {
-        <Item item={item} key={item.objectID} />;
-        );
-      })}
+      {
+        list.map(function (item) {
+          return <Item item=item key=item.objectID />;
+        })
+      }
     </ul>
   );
 }
@@ -56,7 +57,7 @@ function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      
+
       <Search />
 
       <hr />
