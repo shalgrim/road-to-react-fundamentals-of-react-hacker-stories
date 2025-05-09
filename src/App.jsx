a20c7eb85c6c51c9c7bdb17e63490afb641e5bf8
Scott Halgrim <shalgrim@gmail.com>
2025-05-08 22:12:39 -0700

    p113 but why is search #1 not working anymore

--- src/App.jsx
+++ src/App.jsx
@@ -56,33 +56,36 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 }
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
+const getAsyncStories = () =>
+  new Promise((resolve) =>
+    setTimeout(
+      () => resolve({ data: { stories: initialStories } }),
+      2000
+    )
+  );
+
 
 const App = () => {
   console.log("App renders");
-  const initialStories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1
-    }
-  ];
-
-  const getAsyncStories = () =>
-    new Promise((resolve) =>
-      resolve({ data: { stories: initialStories } })
-    );
-
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
   const handleSearch = (event) => {
