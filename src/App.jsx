591f425b2a6fb2f4681de487f1066c470e5501d9
Scott Halgrim <shalgrim@gmail.com>
2025-06-08 11:32:56 -0700

    end Forms in React

--- src/App.jsx
+++ src/App.jsx
@@ -146,25 +146,44 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDefault():
   };
+
+  const SearchForm = ({
+    searchTerm,
+    onSearchInput,
+    onSearchSubmit,
+  }) => (
+    <form onSubmit={onSearchSubmit}>
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={onSearchInput}
+        >
+          <strong>Search:</strong>
+          </InputWithLabel>
+
+        <button
+          type="submit"
+          disabled={!searchTerm}
+        >
+          Submit
+        </button>
+    </form>
+  );
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel value={searchTerm} onInputChange={handleSearchInput} id="search1">
-        <strong>Search:</strong>
-      </InputWithLabel>
-
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
+      <SearchForm
+      searchTerm={searchTerm}
+      onSearchInput={handleSearchInput}
+      onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
 
