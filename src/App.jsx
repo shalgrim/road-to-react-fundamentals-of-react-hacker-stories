61f12abb2b5e17d8b3e057f01d6624147b572927
Scott Halgrim <shalgrim@gmail.com>
2025-06-14 13:49:40 -0700

    end p183 but something is broken

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,107 @@
 /* eslint-disable no-unused-vars */
 import * as React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
+const StyledContainer = styled.div`
+  height: 100vw;
+  padding: 20px;
+
+  background: #83a4d4;
+  background: linear-gradient(to left, #b6fbff, #83a4d4);
+
+  color: #171212;
+
+`;
+
+const StyledHeadlinePrimary = styled.h1`
+  font-size: 48px;
+  font-weight: 300;
+  letter-spacing: 2px;
+`;
+
+const StyledItem = styled.li`
+  display: flex;
+  align-items: center;
+  padding-bottom: 5px;
+`;
+
+const StyledColumn = styled.span`
+  padding: 0 5px;
+  white-space: nowrap;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis
+  
+  a { 
+    color: inherit;
+  }
+  
+  width: ${(props) => props.width};
+`;
+
+const StyledButton = styled.button`
+  background: transparent;
+  border: 1px solid #171212;
+  padding: 5px;
+  cursor: pointer;
+
+  transition: all 0.1s ease-in;
+
+  &.hover {
+    background: #171212;
+    color: #ffffff;
+  }
+`;
+
+const StyledButtonSmall = styled(StyledButton)`
+  padding: 5px;
+`;
+
+const StyledButtonLarge = styled(StyledButton)`
+  padding: 10px;
+`;
+
+const StyledSearchForm = styled.form`
+  padding: 10px 0 20px 0;
+  dislay: flex;
+  align-items: baseline;
+`;
+
+const StyledLabel = styled.label`
+  border-top: 1px solid #171212;
+  border-left: 1px solid #171212;
+  padding-left: 5px;
+  font-size: 24px;
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  border-bottom: 1px solid #171212;
+  background-color: transparent;
+
+  font-size: 24px;
+`;
+
 const Item = ({ item, onRemoveItem }) => {
-  return (
-    <li>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" onClick={() => onRemoveItem(item)}>
-          Dismiss
-        </button>
-      </span>
-    </li>
-  );
+  <StyledItem>
+    <StyledColumn width="40%">
+      <a href={item.url}>{item.title}</a>
+    </StyledColumn>
+    <StyledColumn width="30%">{item.author}</StyledColumn>
+    <StyledColumn width="10%">{item.num_comments}</StyledColumn>
+    <StyledColumn width="10%">{item.points}</StyledColumn>
+    <StyledColumn width="10%">
+      <StyledButtonSmall
+        type="button"
+        onClick={() => onRemoveItem(item)}
+      >
+        Dismiss
+      </StyledButtonSmall>
+    </StyledColumn>
+  </StyledItem>
 };
 
 const List = ({ list, onRemoveItem }) => {
@@ -46,9 +128,15 @@ const InputWithLabel = ({ id, value, type = 'text', isFocused, onInputChange, ch
 
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <StyledLabel htmlFor={id}>{children}</StyledLabel>
       &nbsp;
-      <input ref={inputRef} id={id} type={type} value={value} autoFocus={isFocused} onChange={onInputChange} />
+      <StyledInput
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
       &nbsp;
     </>
   );
@@ -158,7 +246,7 @@ const App = () => {
     onSearchInput,
     searchAction,
   }) => (
-    <form action={searchAction}>
+    <StyledSearchForm onSubmit={searchAction}>
       <InputWithLabel
         id="search"
         value={searchTerm}
@@ -168,18 +256,18 @@ const App = () => {
         <strong>Search:</strong>
       </InputWithLabel>
 
-      <button
+      <StyledButtonLarge
         type="submit"
         disabled={!searchTerm}
       >
         Submit
-      </button>
-    </form>
+      </StyledButtonLarge>
+    </StyledSearchForm>
   );
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <StyledContainer>
+      <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
 
       <SearchForm
         searchTerm={searchTerm}
@@ -208,7 +296,7 @@ const App = () => {
 
       <h2>{searchTerm2}</h2>
 
-    </div>
+    </StyledContainer>
   );
 };
 
