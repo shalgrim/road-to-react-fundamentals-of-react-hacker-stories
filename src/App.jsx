a06fbde87b462219820b40fbe2aa81744ea90bbf
Scott Halgrim <shalgrim@gmail.com>
2025-06-14 10:58:17 -0700

    end p173

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@
 import * as React from 'react';
 import axios from 'axios';
 
+import styles from './App.module.css';
+
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const Item = ({ item, onRemoveItem }) => {
   return (
-    <li>
-      <span>
+    <li className={styles.item}>
+      <span style={{ width: '40%' }}>
         <a href={item.url}>{item.title}</a>
       </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" onClick={() => onRemoveItem(item)}>
+      <span style={{ width: '30%' }}>{item.author}</span>
+      <span style={{ width: '10%' }}>{item.num_comments}</span>
+      <span style={{ width: '10%' }}>{item.points}</span>
+      <span style={{ width: '10%' }}>
+        <button
+          type="button"
+          onClick={() => onRemoveItem(item)}
+          className={`${styles.button} ${styles.buttonSmall}`}
+        >
           Dismiss
         </button>
       </span>
@@ -178,8 +184,8 @@ const App = () => {
   );
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className={styles.container}>
+      <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
