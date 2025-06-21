52d12eae7f6cadf72b9dcfbe454100c76c501fcb
Scott Halgrim <shalgrim@gmail.com>
2025-06-21 10:55:13 -0700

    p252 add list headers

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,13 @@ const List = ({ list, onRemoveItem }) => {
       </div>
       <div>
         <ul>
+          <li style={{ display: 'flex' }}>
+            <span style={{ width: '40%' }}>Title</span>
+            <span style={{ width: '30%' }}>Author</span>
+            <span style={{ width: '10%' }}>Comments</span>
+            <span style={{ width: '10%' }}>Points</span>
+            <span style={{ width: '10%' }}>Actions</span>
+          </li>
           {_.orderBy(list,
             [sortState.sortBy],
             [sortState.sortOrder]
